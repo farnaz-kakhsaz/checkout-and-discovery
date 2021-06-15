a0cbb07598d16b1c42beab8edd01a2893bfdd908
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-06-16 02:57:43 +0430

    add dead emoji icon to empty info card

--- src/components/card/card.compenent.jsx
+++ src/components/card/card.compenent.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 // Images
 import { ReactComponent as InfoIcon } from "../../assets/images/icon-info.svg";
 import { ReactComponent as StarIcon } from "../../assets/images/icon-star.svg";
+import { ReactComponent as DeadEmojiIcon } from "../../assets/images/icon-dead-emoji.svg";
 // CSS
 import "./card.styles.css";
 
@@ -16,19 +17,21 @@ export default function Card({ id, images, title, rating, price, discount }) {
         <div className="info-icon-container">
           <InfoIcon className="info-icon" />
           <div className="info-icon-content">
-            {rating.count ? (
+            {rating.count !== 0 && rating.count ? (
               <>
                 <StarIcon />
                 <div className="rate">{rating.rate}</div>
                 <div className="count">({rating.count})</div>
               </>
             ) : (
-              ""
+              price.selling_price === price.rrp_price && (
+                <DeadEmojiIcon className="dead-emoji-icon" />
+              )
             )}
             {price.selling_price !== price.rrp_price && (
               <div className="discount">
-                <div className="discount-percent">%{discount}</div> تخفیف شما از
-                این خرید!
+                <div className="discount-percent">%{discount}</div>
+                تخفیف شما از این خرید!
               </div>
             )}
           </div>
