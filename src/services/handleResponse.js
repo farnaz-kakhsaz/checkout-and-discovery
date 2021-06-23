1bc683c33f3a843245980f87f98b360278880421
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-06-23 15:45:50 +0430

    replace React env instead of real URL and token

--- src/services/handleResponse.js
+++ src/services/handleResponse.js
@@ -1,10 +1,10 @@
 const getProductsListPage = async (pageNumber, searchKeyword) => {
   try {
     const response = await fetch(
-      `${process.env.REACT_APP_HEROKU ? process.env.REACT_APP_HEROKU : ""}https://www.digikala.com/front-end/search/?page=${pageNumber}&rows=20&price[min]=0&price[max]=100000&has_selling_stock=1&sort=4&q=${searchKeyword}`,
+      `${process.env.REACT_APP_SERVER_URL_PRODUCTS_LIST_PAGE}?page=${pageNumber}&rows=20&price[min]=0&price[max]=100000&has_selling_stock=1&sort=4&q=${searchKeyword}`,
       {
         headers: {
-          token: "mpfKW9ghVTCSuBZ7qTkSmEyvL38ShZxv",
+          token: process.env.REACT_APP_TOKEN,
           "Content-Type": "application/json",
         },
       }
@@ -24,10 +24,10 @@ const getProductsListPage = async (pageNumber, searchKeyword) => {
 const getProductDetailsPage = async (productId) => {
   try {
     const response = await fetch(
-      `${process.env.REACT_APP_HEROKU ? process.env.REACT_APP_HEROKU : ""}https://www.digikala.com/front-end/product/${productId}`,
+      `${process.env.REACT_APP_HEROKU}${process.env.REACT_APP_SERVER_URL_PRODUCT_DETAILS_PAGE}${productId}`,
       {
         headers: {
-          token: "mpfKW9ghVTCSuBZ7qTkSmEyvL38ShZxv",
+          token: process.env.REACT_APP_TOKEN,
           "Content-Type": "application/json",
         },
       }
